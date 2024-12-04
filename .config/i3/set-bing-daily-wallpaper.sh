d7e645d4ae513e8faebce63bd5f1b5980c016199
jonas <jonas@DISTRIB_ID=Ubuntu.com>
2024-12-04 12:56:48 +0100

    Wallpaper is optained from bing

---
+++ .config/i3/set-bing-daily-wallpaper.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+urlpath=$( \
+curl "https://www.bing.com/HPImageArchive.aspx?resolution=UHD&format=rss&idx=0&n=1&mkt=de-de" \
+| xmllint --xpath "/rss/channel/item/link/text()" - \
+| sed 's/1366x768/1920x1080/g' \
+)
+curl "https://www.bing.com$urlpath" \
+| feh --bg-fill -
+
