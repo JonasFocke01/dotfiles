ab5b564033a4d5bc531d73cbe722c69e4fae93e5
JonasFocke01 <jonas-focke@mailbox.org>
2024-12-07 18:24:07 +0100

    Generate new lockscreen image on suspend via ulauncher
    At least its something...
    This should in the far future be something to be executed via systemd on
    suspend. So that it also generates when e.g. the laptop lid closes...

---
+++ .config/i3lock/script_lock_prepare_lockimage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This is a placeholder for the jit prepare script
+#
+rm /tmp/raw_screen.png
+flameshot full -c -p /tmp/raw_screen.png
+convert /tmp/raw_screen.png -blur 0x20 /tmp/blured_screen.png
+convert /tmp/blured_screen.png $HOME/.config/i3lock/lock.png -gravity NorthWest -composite -matte /home/jonas/.config/i3lock/lockscreen.png
