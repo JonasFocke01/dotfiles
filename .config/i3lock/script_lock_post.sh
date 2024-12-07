36751751381bc36eef6b057a1f013fd34a6fc3ed
Jonas_Focke <jonas-focke@mailbox.org>
2024-12-07 18:48:39 +0100

    Add comment

--- .config/i3lock/script_lock_post.sh
+++ .config/i3lock/script_lock_post.sh
@@ -4,6 +4,7 @@ i3lock -c 181818 -i $HOME/.config/i3lock/lockscreen.png -t -n
 
 rm /tmp/raw_screen.png /tmp/raw_screen.png /tmp/lockscreen.png
 
+# TODO: this needs to happen in some sort of a systemd script
 DISPLAY_NAME=$(xrandr | grep ' connected' | awk '{print $1}')
 xrandr --output $DISPLAY_NAME --off
 xrandr --output $DISPLAY_NAME --auto
