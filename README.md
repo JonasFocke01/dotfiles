3f368729d1e1365f3a6f94f1a7ef52064d34c232
JonasFocke01 <jonas-focke@mailbox.org>
2024-12-08 19:30:28 +0100

    Include `debian-installer.sh`

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-The full backup/collection of my dotfiles
+# About
+
+This repo contains three things.
+1. A script to install software i use on any debian-based distro, so that i do not need to remember each step, as some of them require multiple steps to be installed as i need them to be. (version, repository, feature software, ...)
+2. My personal collection of dotfiles to configure the just installed software.
+3. Systemd configurations and unit files i use on any systemd distro
+
+# How to use
+
+To use the full potential of this script, you need to be on a debian-based distro.
+
+- Clone this repo to your home directory.
+`cd $HOME && git clone git@github.com:JonasFocke01/dotfiles.git`
+
+- Make the `debian-installer.sh` executable and, _well_, execute it.
+`cd dotfiles && chmod +x debian-installer.sh && ./install-debian.sh`
+
+This will then, step-by-step, install and adjust the distro to my liking. Again, the script asks before each step, so this wont do things you do not want.
---
+++ install-debian.sh
@@ -0,0 +1,238 @@
+#!/bin/bash
+
+echo "Debian (apt) installer"
+echo
+
+echo "Update System now (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	sudo apt update -y
+	sudo apt upgrade -y
+fi
+
+echo "Install Firefox? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	sudo apt install firefox -y
+fi
+
+echo "create new ssh key? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+    cd $HOME
+	ssh-keygen -f /home/jonas/.ssh/id_rsa -N ''
+	sudo apt install xclip -y
+	echo "Go to the Github login page and log in. The URL is in your clipboard"
+	echo "https://github.com/login" | xclip -sel clip
+	echo "proceed with any key"
+	read confirmation;
+	echo "Now go to the settings and ssh keys. The URL is in your clipboard"
+	echo "https://github.com/settings/keys" | xclip -sel clip
+	echo "proceed with any key"
+	read confirmation;
+	echo "The new ssh key is now in your clipboard, just create a new one on the site."
+	cat .ssh/id_rsa.pub | xclip -sel clip
+	echo "proceed with any key"
+	read confirmation;
+	cat .ssh/id_rsa.pub | xclip -sel clip
+	echo "Now we gonna connect to tigerly"
+	echo "The **ONLY** thing you have to do now, after typing in the password, is to 'CRTL+(SHIFT)+V' and then 'CTRL+C'"
+	echo "Tee is started and therefore you DO NOT NEED TO DO ANYTHING ELSE"
+	echo "This will paste the generated ssh key into the authorized_keys file"
+	echo "And therefore allow this computer from now on to connect to tigerly without a password"
+	ssh manfild@tigerly -p 88 -t "cd .ssh; tee authorized_keys; bash --login"
+fi
+
+echo "Install rust? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+	source "$HOME/.cargo/env"
+ 	cargo install cargo-watch
+  	cargo install cargo-deb
+    	cargo install elf2uf2-rs
+      	cargo install rustup target add thumbv6m-none-eabi
+	cargo install probe-rs
+fi
+
+echo "Install shell? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	sudo apt install zsh fzf alacritty -y
+	cargo install exa
+	chsh -s /usr/bin/zsh
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+	sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/alacritty 50
+    sudo rm -r \
+        .bash_history \
+        .bash_logout \
+        .bashrc
+fi
+
+echo "Add shortcut to reboot into windows? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+        echo "Warning: This does not work correctly, so this step is skipped."
+	echo "Please refer to the install script to manually do this."
+ 	echo "TODO: Fix this step. (Or remove it in favour of fixing the ulauncher option)"
+	# windowsBoot=$(sudo efibootmgr -v | grep Windows | grep -o '000[0-9]')
+	# sed "s/0002/$windowsBoot/g" Desktop/zsh-config/.zshrc > Desktop/zsh-config/tmp
+	# mv Desktop/zsh-config/tmp .zshrc
+fi
+
+echo "Install utils? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	sudo apt install tlp libudev-dev -y
+fi
+
+echo "Install i3 custom de? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	sudo apt install -y \
+ 		i3-wm \
+   		i3bar \
+   		i3blocks \
+     	i3lock \
+        network-manager-gnome \
+	 	dunst \
+   		flameshot \
+        feh \
+        imagemagick \
+	 	fzf \
+   		fd-find \
+        ulauncher \
+        pavucontrol \
+	 	copyq \
+   		ranger
+ 	sudo cp dotfiles/systemd/system/screenlock_pre@.service /etc/systemd/system/screenlock_pre@.service
+ 	sudo cp dotfiles/systemd/system/screenlock_post@.service /etc/systemd/system/screenlock_post@.service
+  	sudo systemctl daemon-reload
+ 	sudo systemctl enable screenlock_pre@jonas.service
+  	sudo systemctl enable screenlock_post@jonas.service
+fi
+
+echo "Install neovim? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	sudo add-apt-repository ppa:neovim-ppa/unstable -y
+	sudo apt update -y
+	sudo apt install -y \
+ 		neovim \
+		ripgrep
+fi
+
+echo "Clone WIP projects? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	mkdir $HOME/repos
+ 	cd $HOME/repos
+ 	git clone git@github.com:JonasFocke01/ruhige_waldgeraeusche.git
+  	git clone git@github.com:JonasFocke01/remindy.git
+fi
+
+echo "Install solar-overview? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	mkdir $HOME/repos
+    cd $HOME/repos
+	git clone git@github.com:JonasFocke01/solar-overview.git repos/solar-overview
+	cd $HOME/repos/solar-overview
+	./linux_install_script.sh
+fi
+
+echo "Install Gaming suite? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	sudo apt install -y \
+ 		steam \
+ 		discord
+fi
+
+echo "Automount tigerly? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+    echo "//tigerly/Allgemeine\040Daten /mnt/tigerly cifs username=manfild,password=DSsiKul24Sd,vers=2.0 0 0" | sudo tee -a /etc/fstab
+fi
+
+echo "Install nodeversionmanager? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+fi
+
+echo "Install spotify? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+ 	sudo apt update
+	sudo apt install spotify -y
+fi
+
+echo "Install Vlc Player? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	sudo apt install vlc -y
+fi
+
+# TODO: This should better be a job for Systemd
+# TODO: This looks broken. Investigate
+echo "Automount proton at startup? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	mkdir proton
+	crontab -l > mycron
+ 	echo "TODO: rclone needs to be installed and configured"
+ 	echo "* * * * * rclone mount proton: proton --vfs-cache-mode writes" >> mycron
+  	crontab mycron
+ 	rm mycron
+fi
+
+echo "Configure Systemd config? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+ 	sudo cp dotfiles/systemd/logind.conf /etc/systemd/logind.conf
+fi
+
+echo "Install arduino-cli? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+    cd $HOME/Desktop
+    wget https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz
+    mkdir ../.arduino-cli
+    tar -xzf arduino-cli_latest_Linux_64bit.tar.gz -C ../.arduino-cli
+    cd $HOME
+    sed -i -e "\$s/\$/:\/home\/$(whoami)\/.arduino-cli/" .zshrc
+fi
+
+echo "Use my dotfiles? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+    sudo apt install stow -y
+    cd $HOME/dotfiles
+    stow --adopt .
+    git restore .
+fi
+
+echo "Remove default home dir clutter? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	sudo rm -r \
+        Documents \
+        Music \
+        Pictures \
+        Videos \
+        Public \
+        Templates
+fi
+
+echo "Restart the system now? (y/n)"
+read confirmation;
+if [ $confirmation = "y" ]; then
+	sudo shutdown -r now
+fi
+
+
