51303291955ab981ef82ce8dff82027f8bfb7d42
Jonas_Focke <jonas-focke@mailbox.org>
2024-12-10 19:11:01 +0100

    fetch api only when network is available

--- .config/i3/set-nasa-daily-wallpaper.sh
+++ .config/i3/set-nasa-daily-wallpaper.sh
@@ -3,15 +3,15 @@
 API_KEY=$(cat nasa-api-key)
 API_URL="https://api.nasa.gov/planetary/apod?api_key=${API_KEY}"
 
-response=$(curl -s "$API_URL")
-
-hd_image_url=$(echo "$response" | jq -r '.hdurl')
-
 if [ -e "/tmp/nasa-wallpaper.jpg" ]; then
     feh --bg-fill /tmp/nasa-wallpaper.jpg
 fi
 
 while ! ping -c 1 1.1.1.1 ; do sleep 1 ; done
+
+response=$(curl -s "$API_URL")
+
+hd_image_url=$(echo "$response" | jq -r '.hdurl')
 
 curl -o /tmp/nasa-wallpaper.jpg "$hd_image_url"
 
