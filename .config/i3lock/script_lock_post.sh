2f249edc45dc291887ce3160a46d7e5decd2dd4d
JonasFocke01 <jonas-focke@mailbox.org>
2024-12-11 12:31:01 +0100

    Reenable screen search after sudpend fix

--- .config/i3lock/script_lock_post.sh
+++ .config/i3lock/script_lock_post.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 i3lock -c 181818 -i /tmp/lockscreen.png -t -n
+
+# TODO: this needs to happen in some sort of a systemd script
+DISPLAY_NAME=$(xrandr | grep ' connected' | awk '{print $1}')
+xrandr --output $DISPLAY_NAME --off
+xrandr --output $DISPLAY_NAME --auto
