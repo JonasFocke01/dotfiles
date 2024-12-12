919f9b72b13361494e9a4087d187462527a35327
Jonas_Focke <jonas-focke@mailbox.org>
2024-12-12 22:13:56 +0100

    adjust keybindings

--- .config/nvim/after/plugin/smart-open.lua
+++ .config/nvim/after/plugin/smart-open.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>e", function()
+vim.keymap.set("n", "<leader><leader>", function()
     require("telescope").extensions.smart_open.smart_open({
         cwd_only = true,
     })
