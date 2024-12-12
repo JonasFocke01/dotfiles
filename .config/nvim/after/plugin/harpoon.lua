f9097c25b2969b9ed063f24ed3a9220e7481c946
Jonas_Focke <jonas-focke@mailbox.org>
2024-12-12 22:15:03 +0100

    adjust keybindings

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -5,7 +5,7 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>+", function() harpoon:list():add() end)
-vim.keymap.set("n", "<leader><leader>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<leader>l", function() harpoon:list():prev() end)
