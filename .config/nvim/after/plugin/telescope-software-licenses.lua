a02d0fb0f91742163b3693cab2f804b2ecdf27b7
Jonas_Focke <jonas-focke@mailbox.org>
2024-12-12 21:50:03 +0100

    add todo comment

--- .config/nvim/after/plugin/telescope-software-licenses.lua
+++ .config/nvim/after/plugin/telescope-software-licenses.lua
@@ -1,15 +1,1 @@
-vim.keymap.set("n", "<leader>MIT", function()
-    local buffer = vim.api.nvim_get_current_buf();
-    local buffer_name = vim.api.nvim_buf_get_name(buffer);
-    if string.find(buffer_name, "LICENSE") then
-        vim.cmd('Telescope software-licenses find')
-    else
-        local cwd = vim.fn.getcwd()
-        local file_name = cwd .. "/LICENSE"
-        local file = io.open(file_name, "w");
-        file:write("");
-        file:close()
-        vim.cmd('edit ' .. file_name)
-        vim.cmd('Telescope software-licenses find')
-    end
-end);
+-- TODO: remove file
