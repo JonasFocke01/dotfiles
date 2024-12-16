748254478ffb3067229da7612133681fd459d27a
JonasFocke01 <jonas-focke@mailbox.org>
2024-12-16 13:23:49 +0100

    Remove unused flameshot flag

--- .config/i3lock/script_lock_prepare_lockimage.sh
+++ .config/i3lock/script_lock_prepare_lockimage.sh
@@ -3,6 +3,6 @@
 # This is a temporary replacement for `script-lock-pre.sh`.
 #
 rm /tmp/raw_screen.png
-flameshot full -c -p /tmp/raw_screen.png
+flameshot full -p /tmp/raw_screen.png
 convert /tmp/raw_screen.png -blur 0x20 /tmp/blured_screen.png
 convert /tmp/blured_screen.png $HOME/.config/i3lock/lock.png -gravity NorthWest -composite -matte /tmp/lockscreen.png
