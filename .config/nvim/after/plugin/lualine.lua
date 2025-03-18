287ddaad7d1d7a05d0dbe3dc72c2eef2bbee30ac
JonasFocke01 <jonas-focke@mailbox.org>
2025-03-18 13:31:54 +0100

    blanklines should also show

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -40,34 +40,47 @@ local debounce_result_codeline_num = ''
 local function codeline_num()
     if debounce_codeline_num == 255 then
         if string.find(get_root_dir(), "repos") or string.find(get_root_dir(), ".config") then
+            -- Remember to add new filetypes or directorys to BOTH `rg` calls
+            -- To add new filetypes, just add a line containing `"-g '*.xy' " ..`
+            local rg_include_files = "" ..
+                "-g '*.rs' " ..
+                "-g '*.c' " ..
+                "-g '*.h' " ..
+                "-g '*.cpp' " ..
+                "-g '*.cs' " ..
+                "-g '*.ino' " ..
+                "-g '*.sh' " ..
+                "-g '*.py' " ..
+                "-g '*.svelte' " ..
+                "-g '*.lua' " ..
+                "-g '*.vue' " ..
+                "-g '*.md' " ..
+                "-g '*.js' " ..
+                "-g '*.ts' " ..
+                "-g '*.go' "
+            -- To exclude a dir, just add a line containing `"-g '!*/xyz/*' " ..`
+            local rg_exclude_dirs = "" ..
+                "-g '!*/gdtf_parser/*' " ..
+                "-g '!gdtf_parser/*' " ..
+                "-g '!shows/*' "
+            local empty_line_count = io.popen(
+                    "rg --files " ..
+                    rg_include_files ..
+                    rg_exclude_dirs ..
+                    "| xargs cat 2>/dev/null " ..
+                    "| grep -c '^$'")
+                :read("*n")
             local line_count = io.popen(
                     "rg --files " ..
-                    -- To add new filetypes, just add a line containing `"-g '*.xy' " ..`
-                    "-g '*.rs' " ..
-                    "-g '*.c' " ..
-                    "-g '*.h' " ..
-                    "-g '*.cpp' " ..
-                    "-g '*.cs' " ..
-                    "-g '*.ino' " ..
-                    "-g '*.sh' " ..
-                    "-g '*.py' " ..
-                    "-g '*.svelte' " ..
-                    "-g '*.lua' " ..
-                    "-g '*.vue' " ..
-                    "-g '*.md' " ..
-                    "-g '*.js' " ..
-                    "-g '*.ts' " ..
-                    "-g '*.go' " ..
-                    -- To exclude a dir, just add a line containing `"-g '!*/xyz/*' " ..`
-                    "-g '!*/gdtf_parser/*' " ..
-                    "-g '!gdtf_parser/*' " ..
-                    "-g '!shows/*' " ..
+                    rg_include_files ..
+                    rg_exclude_dirs ..
                     "| xargs wc -l 2>/dev/null " ..
                     "| tail -n1 " ..
                     "| awk 'END {print $1}'")
                 :read(
                     "*n")
-            debounce_result_codeline_num = line_count .. " lines"
+            debounce_result_codeline_num = line_count ..
+                " lines (" .. string.format("%.0f", (empty_line_count / line_count) * 100) .. "%% blank)"
             debounce_codeline_num = 0
             return debounce_result_codeline_num
         else
