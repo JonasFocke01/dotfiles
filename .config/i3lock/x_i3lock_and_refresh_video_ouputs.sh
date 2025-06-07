0c136339bbed9e4268bf7c593a257cd655f6ad78
JonasFocke01 <jonas-focke@mailbox.org>
2025-06-07 18:53:15 +0200

    Refactor i3lock action

--- .config/i3lock/x_i3lock_and_refresh_video_ouputs.sh
+++ .config/i3lock/x_i3lock_and_refresh_video_ouputs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+# This script is currently invoked by zzz -> resume (symlinked into `/etc/zzz.d/resume/`)
 
 i3lock -c 181818 -i /tmp/lockscreen.png -t -n
 
-# TODO: this needs to happen in some sort of a systemd script
 DISPLAY_NAME=$(xrandr | grep ' connected' | awk '{print $1}')
 xrandr --output $DISPLAY_NAME --off
 xrandr --output $DISPLAY_NAME --auto
