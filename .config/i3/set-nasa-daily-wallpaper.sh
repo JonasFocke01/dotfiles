16b07c44a0a293ddde784fe59e3bb39e50a7aae2
JonasFocke01 <jonas-focke@mailbox.org>
2025-10-07 21:57:28 +0200

    Improve nasa-daily-wallpaper

--- .config/i3/set-nasa-daily-wallpaper.sh
+++ .config/i3/set-nasa-daily-wallpaper.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-API_KEY=$(cat nasa-api-key)
-API_URL="https://api.nasa.gov/planetary/apod?api_key=${API_KEY}"
+if [ -e "$HOME/nasa-api-key" ]; then
+    API_KEY=$(cat $HOME/nasa-api-key)
+    API_URL="https://api.nasa.gov/planetary/apod?api_key=${API_KEY}"
 
-if [ -e "/tmp/nasa-wallpaper.jpg" ]; then
-    feh --bg-fill /tmp/nasa-wallpaper.jpg
+    if [ -e "$HOME/nasa-wallpaper.jpg" ]; then
+        feh --bg-fill $HOME/nasa-wallpaper.jpg
+    fi
+
+    while ! ping -c 1 1.1.1.1 ; do sleep 1 ; done
+
+    response=$(curl -s "$API_URL")
+
+    hd_image_url=$(echo "$response" | jq -r '.hdurl')
+
+    curl -o $HOME/nasa-wallpaper.jpg "$hd_image_url"
+else
+    magick -size 1920x1080 xc:black -fill white -pointsize 50 -gravity center -draw "text 0,0 'No nasa api key found (expecting file \`\$HOME/nasa-api-key\`)'" $HOME/nasa-wallpaper.jpg
 fi
 
-while ! ping -c 1 1.1.1.1 ; do sleep 1 ; done
-
-response=$(curl -s "$API_URL")
-
-hd_image_url=$(echo "$response" | jq -r '.hdurl')
-
-curl -o /tmp/nasa-wallpaper.jpg "$hd_image_url"
-
-feh --bg-fill /tmp/nasa-wallpaper.jpg
+feh --bg-fill $HOME/nasa-wallpaper.jpg
