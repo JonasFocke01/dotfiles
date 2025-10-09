e0a4ca488d9a726f53312e789449e95c0f5dc119
JonasFocke01 <jonas-focke@mailbox.org>
2025-10-09 22:39:30 +0200

    Firefox script now REALY REALY works

--- .config/i3/start_firefox.sh
+++ .config/i3/start_firefox.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 firefox &
+export I3SOCK=$(i3 --get-socket)
 
 # gaming
 while true; do
-    id=$(wmctrl -l -x | grep 'firefox.\+gaming' | grep -v 'Alacritty' | awk '{print $1}')
+    id=$(wmctrl -l -x | grep 'Firefox.\+gaming' | grep -v 'Alacritty' | awk '{print $1}')
 
     if [ -n "$id" ]; then
 i3-msg "[id=$id] move container to workspace 󰺷"
@@ -17,7 +18,7 @@ done
 
 # music
 while true; do
-    id=$(wmctrl -l -x | grep 'firefox.\+music' | grep -v 'Alacritty' | awk '{print $1}')
+    id=$(wmctrl -l -x | grep 'Firefox.\+music' | grep -v 'Alacritty' | awk '{print $1}')
 
     if [ -n "$id" ]; then
 i3-msg "[id=$id] move container to workspace "
@@ -30,7 +31,7 @@ done
 
 # work_1
 while true; do
-    id=$(wmctrl -l -x | grep 'firefox.\+work_1' | grep -v 'Alacritty' | awk '{print $1}')
+    id=$(wmctrl -l -x | grep 'Firefox.\+work_1' | grep -v 'Alacritty' | awk '{print $1}')
 
     if [ -n "$id" ]; then
 i3-msg "[id=$id] move container to workspace "
@@ -42,7 +43,7 @@ done
 
 # work_2
 while true; do
-    id=$(wmctrl -l -x | grep 'firefox.\+work_2' | grep -v 'Alacritty' | awk '{print $1}')
+    id=$(wmctrl -l -x | grep 'Firefox.\+work_2' | grep -v 'Alacritty' | awk '{print $1}')
 
     if [ -n "$id" ]; then
 i3-msg "[id=$id] move container to workspace "
@@ -54,7 +55,7 @@ done
 
 # work_3
 while true; do
-    id=$(wmctrl -l -x | grep 'firefox.\+work_3' | grep -v 'Alacritty' | awk '{print $1}')
+    id=$(wmctrl -l -x | grep 'Firefox.\+work_3' | grep -v 'Alacritty' | awk '{print $1}')
 
     if [ -n "$id" ]; then
 i3-msg "[id=$id] move container to workspace "
