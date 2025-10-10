7f7865476676bb24fa2291c3b13b2d0facdffefc
JonasFocke01 <jonas-focke@mailbox.org>
2025-10-10 16:16:16 +0200

    nasa-wallpaper-setter is not more linux compliant

--- .config/i3/set-nasa-daily-wallpaper.sh
+++ .config/i3/set-nasa-daily-wallpaper.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-if [ -e "$HOME/nasa-api-key" ]; then
-    API_KEY=$(cat $HOME/nasa-api-key)
+wallpaper_path="$HOME/.cache/nasa-wallpaper.jpg"
+
+mkdir -p $(dirname "$wallpaper_path")
+
+if [ -e "$HOME/.config/nasa-api-key" ]; then
+    API_KEY=$(cat $HOME/.config/nasa-api-key)
     API_URL="https://api.nasa.gov/planetary/apod?api_key=${API_KEY}"
 
-    if [ -e "$HOME/nasa-wallpaper.jpg" ]; then
-        feh --bg-fill $HOME/nasa-wallpaper.jpg
+    if [ -e "$wallpaper_path" ]; then
+        feh --bg-fill "$wallpaper_path"
     fi
 
     while ! ping -c 1 1.1.1.1 ; do sleep 1 ; done
@@ -14,9 +18,9 @@ if [ -e "$HOME/nasa-api-key" ]; then
 
     hd_image_url=$(echo "$response" | jq -r '.hdurl')
 
-    curl -o $HOME/nasa-wallpaper.jpg "$hd_image_url"
+    curl -o $wallpaper_path "$hd_image_url"
 else
-    magick -size 1920x1080 xc:black -fill white -pointsize 50 -gravity center -draw "text 0,0 'No nasa api key found (expecting file \`\$HOME/nasa-api-key\`)'" $HOME/nasa-wallpaper.jpg
+    magick -size 1920x1080 xc:black -fill white -pointsize 50 -gravity center -draw "text 0,0 'No nasa api key found (expecting file \`\$HOME/.config/nasa-api-key\`)'" "$wallpaper_path"
 fi
 
-feh --bg-fill $HOME/nasa-wallpaper.jpg
+feh --bg-fill "$wallpaper_path"
