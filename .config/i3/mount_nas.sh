d7e1467c5c76d8b9b9b9b1b65e40ef5210e28ef9
JonasFocke01 <jonas-focke@mailbox.org>
2025-11-06 00:43:03 +0100

    Bump cifs version

--- .config/i3/mount_nas.sh
+++ .config/i3/mount_nas.sh
@@ -16,6 +16,7 @@ sudo chown $(sudo -u $(whoami) whoami) "/mnt/tigerly" -R
 
 while ! ping -c 1 1.1.1.1 ; do sleep 1 ; done
 
-sudo mount -t cifs "//$nas_ip/Allgemeine Daten" "$data_dir" -o username=$nas_user,password=$nas_pass,uid=$(id -u),gid=$(id -g),vers=1.0
-sudo mount -t cifs "//$nas_ip/docker" "$docker_dir" -o username=$nas_user,password=$nas_pass,uid=$(id -u),gid=$(id -g),vers=1.0
-sudo mount -t cifs "//$nas_ip/video" "$video_dir" -o username=$nas_user,password=$nas_pass,uid=$(id -u),gid=$(id -g),vers=1.0
+sudo mount -t cifs "//$nas_ip/Allgemeine Daten" "$data_dir" -o username=$nas_user,password=$nas_pass,uid=$(id -u),gid=$(id -g),vers=2.0
+sudo mount -t cifs "//$nas_ip/Allgemeine Daten/Familie/Jonas/Desktop" "$HOME/Desktop" -o username=$nas_user,password=$nas_pass,uid=$(id -u),gid=$(id -g),vers=2.0
+sudo mount -t cifs "//$nas_ip/docker" "$docker_dir" -o username=$nas_user,password=$nas_pass,uid=$(id -u),gid=$(id -g),vers=2.0
+sudo mount -t cifs "//$nas_ip/video" "$video_dir" -o username=$nas_user,password=$nas_pass,uid=$(id -u),gid=$(id -g),vers=2.0
